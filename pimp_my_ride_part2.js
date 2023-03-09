bd840273d8cb74fee8cfdd0e73bf7f670abcd626
Jean-JacquesValmont <jeanjacquesvalmont@gmail.com>
2023-03-09 10:54:19 +0100

    Partie 2 finish

--- pimp_my_ride_part2.js
+++ pimp_my_ride_part2.js
@@ -1,5 +1,3 @@
-let trip = "Perdita 8 10 8"
-
 class Trip {
 
     constructor(name,start,duration,price){
@@ -10,20 +8,44 @@ class Trip {
     }
 }
 
+let tripsToParse = [
+    "Roger 0 5 10",
+    "Pongo 3 7 14",
+    "Perdita 8 10 8",
+    "Anita 16 3 7"
+]
+
+//let trip = "Perdita 8 10 8"
+array_ticket = []
+
 function parseTrip(trip) {
     let trip_object = new Trip()
-    let trip_split = trip.split(' ')
+    let trip_index = trip[i]
+    let trip_split = trip_index.split(' ')
 
     trip_object.name = trip_split[0]
     trip_object.start = parseInt(trip_split[1])
     trip_object.duration = parseInt(trip_split[2])
     trip_object.price = parseInt(trip_split[3])
 
+    array_ticket[i] = trip_object
+
     console.log(trip_object)
-    console.log(trip_object.name)
-    console.log(trip_object.start)
-    console.log(trip_object.duration)
-    console.log(trip_object.price)
+    // console.log(trip_object.name)
+    // console.log(trip_object.start)
+    // console.log(trip_object.duration)
+    // console.log(trip_object.price)
+
+    return array_ticket
 }
 
-parseTrip(trip)
+function parseTrips(trips) {
+
+    for (i = 0; i < trips.length; i++)
+    {
+        parseTrip(trips)
+    }
+    console.log(array_ticket) 
+}
+
+parseTrips(tripsToParse)
